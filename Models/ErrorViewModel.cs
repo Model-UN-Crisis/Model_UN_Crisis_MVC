f2671666d74a0d92514b0a783bccb1ae1817307e
Gregory <Gregory@GREGORYS-LAPTOP>
2024-03-19 17:30:41 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Model_UN_Crisis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
