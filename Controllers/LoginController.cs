ede8c5a1d2b0086db0c5a784b9d1fe09e9ad0967
Vincent <100973751+vincentkipchoge@users.noreply.github.com>
2024-04-06 21:52:26 -0400

    Login form page

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Model_UN_Crisis.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
