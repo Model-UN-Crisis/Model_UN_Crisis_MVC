27ac874393922cbf224b99be4a807a1445d13f3b
Vincent <100973751+vincentkipchoge@users.noreply.github.com>
2024-04-10 04:06:01 -0400

    Message Hub view design template // Requires backend functionality implementation

---
+++ Controllers/MessageHubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Model_UN_Crisis.Controllers
+{
+    public class MessageHubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
