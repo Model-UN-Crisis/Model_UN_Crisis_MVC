231089ca698ee8e8ddfd327a2bc745773c177685
James Ritzi <jritzi@students.kennesaw.edu>
2024-04-24 16:25:40 -0400

    James Ritiz - 9: Update

---
+++ Controllers/NewsHubController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model_UN_Crisis.DAL;
+using Model_UN_Crisis.Models;
+using System.Text.RegularExpressions;
+using System.Timers;
+
+namespace Model_UN_Crisis.Controllers
+{
+    public class NewsHubController : Controller
+    {
+        private readonly ModelUNDbContext modelUNDbContext;
+
+        public NewsHubController(ModelUNDbContext _modelUNDbContext)
+        {
+            this.modelUNDbContext = _modelUNDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index(STG_News model)
+        {
+            try 
+            {
+                var news = modelUNDbContext.STG_News.ToList();
+                return View(news);
+            } 
+            catch (Exception ex)
+            { 
+                return View(model);
+            }
+        }
+    }
+}
